ab8ad824892d2241ee04fb170767041384c00ed7
David Li <zdavidli@gmail.com>
2019-04-16 17:25:46 -0400

    script to run on colab

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
-import torch
 import os
+
+import torch
 
 # Training arguments
 image_channels = 3
---
+++ tool_presence.py
@@ -0,0 +1,119 @@
+import os
+
+import numpy as np
+import torch
+import torch.optim as optim
+from matplotlib import pyplot as plt
+from src import constants as c
+from src import utils
+from src import visualization as v
+from src import model as m
+from torchvision.utils import save_image
+from tqdm import tqdm, trange
+
+
+def main(args):
+    datasets, dataloaders = utils.setup_data(args)
+    args.betas = [float(x) for x in args.betas.split(',')]
+    for beta in tqdm(args.betas):
+        output_name = 'beta_{}_epoch_{}.{}'
+        losses = {'kl': [], 'rl': []}
+        model = m.VAE(image_channels=args.image_channels,
+                      image_size=args.image_size,
+                      h_dim1=1024,
+                      h_dim2=128,
+                      zdim=args.z_dim).to(c.device)
+
+        optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+        loss_params = {'input_size': args.image_size,
+                       'zdim': args.z_dim,
+                       'beta': beta}
+
+        tbar = trange(args.epochs)
+        for epoch in tbar:
+            """
+            Training
+            """
+            model.train()
+            train_loss, kl, rl = 0, 0, 0
+            t2 = tqdm(dataloaders['train'])
+            for batch_idx, (data, _) in enumerate(t2):
+                data = data.to(c.device)
+                optimizer.zero_grad()
+                recon_batch, mu, logvar = model(data)
+                loss, r, k = m.loss_function(recon_batch,
+                                             data,
+                                             mu,
+                                             logvar,
+                                             **loss_params)
+                loss.backward()
+
+                train_loss += loss.item()
+                kl += k.item()
+                rl += r.item()
+
+                optimizer.step()
+
+                t2.set_postfix(
+                    {"Reconstruction Loss": r.item(),
+                     "KL Divergence": k.item()})
+
+            losses['kl'].append(kl)
+            losses['rl'].append(rl)
+
+            tbar.set_postfix({"KL Divergence":
+                              kl/len(dataloaders['train'].dataset),
+                              "Reconstruction Loss":
+                              rl/len(dataloaders['train'].dataset)})
+
+            """
+            Testing
+            """
+            if (epoch + 1) % args.sample_model_interval == 0:
+                model.eval()
+                with torch.no_grad():
+                    it = iter(dataloaders['val'])
+                    data, _ = next(it)
+                    data = data.to(c.device)
+                    recon_batch, mu, logvar = model(data)
+                    loss, r, k = m.loss_function(
+                        recon_batch, data, mu, logvar, **loss_params)
+                    n = min(data.size(0), 8)
+                    comparison = torch.cat([data[:n],
+                                            recon_batch.view(args.batch_size,
+                                                             c.image_channels,
+                                                             args.image_size,
+                                                             args.image_size)
+                                            [:n]
+                                            ])
+
+                    save_image(comparison.cpu(),
+                               os.path.join(args.output_dir,
+                                            output_name.format(
+                                                beta,
+                                                epoch+1,
+                                                'png')
+                                            ),
+                               nrow=n)
+
+            if (epoch + 1) % args.save_model_interval == 0:
+                torch.save(model.state_dict(),
+                           os.path.join(args.output_dir,
+                                        output_name.format(beta,
+                                                           epoch+1,
+                                                           'torch')))
+
+
+# set up argparse
+parser = utils.setup_argparse()
+args = parser.parse_args(['--data-dir=./data/surgical_data/',
+                          '--output-dir=./data/beta_vae/2fc',
+                          '--epochs=1',
+                          '--image-size=16',
+                          '--betas=2,5',
+                          '--sample-model-interval=1'])
+args.data_dir = os.path.abspath(args.data_dir)
+os.makedirs(args.output_dir, exist_ok=True)
+
+# pass args to main
+main(args)
