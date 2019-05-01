62aa328373c175957f3654db8c42afc845a30b77
David Li <zdavidli@gmail.com>
2019-05-01 13:19:22 -0400

    fixed src/utils/

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
+from scipy.special import logsumexp
+from scipy.stats import norm
 import argparse
+import numpy as np
 import os
 
 import src.constants as c
@@ -40,7 +43,7 @@ def setup_argparse():
                         default=os.path.abspath('.'),
                         help='Root directory of tool-presence')
     parser.add_argument('--data-dir', type=str,
-                        default=os.path.join(c.data_home, 'surgical_data'),
+                        default=os.path.join(c.data_home, 'youtube_data'),
                         help=' ')
     parser.add_argument('--output-dir', type=str,
                         default='', help='Directory to save outputs')
@@ -82,7 +85,7 @@ def setup_data(args, augmentation=False):
     if augmentation:
         transformations.extend([
             transforms.RandomHorizontalFlip(),
-            transforms.RandomAffine(15),])
+            transforms.RandomAffine(15), ])
 
     transformations.extend([
         transforms.Resize(args.image_size),
@@ -102,3 +105,79 @@ def setup_data(args, augmentation=False):
                    for x in ['train', 'val']}
 
     return image_datasets, dataloaders
+
+
+def compute_samples(data, model, num_samples):
+    """
+    Sample from importance distribution z_samples ~ q(z|X) and
+    compute p(z_samples), q(z_samples) for importance sampling
+    Adapted from http://bjlkeng.github.io/posts/importance-sampling-and-estimating-marginal-likelihood-in-variational-autoencoders/
+    """
+
+    z_mean, z_log_sigma = model.encode(data.to(c.device))
+    z_mean, z_log_sigma = torch_to_numpy(
+        z_mean), torch_to_numpy(z_log_sigma)
+    z_samples = []
+    qz = []
+
+    for m, s in zip(z_mean, z_log_sigma):
+        z_vals = [np.random.normal(m[i], np.exp(s[i]), num_samples)
+                  for i in range(len(m))]
+        qz_vals = [norm.pdf(z_vals[i], loc=m[i], scale=np.exp(s[i]))
+                   for i in range(len(m))]
+        z_samples.append(z_vals)
+        qz.append(qz_vals)
+
+    z_samples = np.array(z_samples)
+    pz = norm.pdf(z_samples)
+    qz = np.array(qz)
+
+    z_samples = np.swapaxes(z_samples, 1, 2)
+    pz = np.swapaxes(pz, 1, 2)
+    qz = np.swapaxes(qz, 1, 2)
+
+    return z_samples, pz, qz
+
+
+def estimate_logpx(dataloader, model, args, num_samples):
+    """
+    Adapted from http://bjlkeng.github.io/posts/importance-sampling-and-estimating-marginal-likelihood-in-variational-autoencoders/
+
+    Calculate importance sample
+    \log p(x) = E_p[p(x|z)]
+    = \log(\int p(x|z) p(z) dz)
+    = \log(\int p(x|z) p(z) / q(z|x) q(z|x) dz)
+    = E_q[p(x|z) p(z) / q(z|x)]
+    ~= \log(1/n * \sum_i p(x|z_i) p(z_i)/q(z_i))
+    = \log p(x) = \log(1/n * \sum_i e^{\log p(x|z_i) + \log p(z_i) - \log q(z_i)})
+    = \log p(x) = -\logn + \logsumexp_i(\log p(x|z_i) + \log p(z_i) - \log q(z_i))
+    See: scipy.special.logsumexp
+    """
+
+    result = []
+    for batch_idx, (data, _) in enumerate(dataloader):
+        z_samples, pz, qz = compute_samples(data, model, num_samples)
+        assert z_samples.shape == pz.shape
+        assert pz.shape == qz.shape
+        for i in range(len(data)):
+            datum = torch_to_numpy(data[i]).reshape(
+                args.image_size * args.image_size * args.image_channels)
+            x_predict = model.decode(torch.from_numpy(
+                z_samples[i]).float().to(c.device))
+            x_predict = torch_to_numpy(
+                x_predict).reshape(-1, args.image_size * args.image_size * args.image_channels)
+            x_predict = np.clip(x_predict, np.finfo(
+                float).eps, 1. - np.finfo(float).eps)
+            p_vals = pz[i]
+            q_vals = qz[i]
+
+            # \log p(x|z) = Binary cross entropy
+            logp_xz = np.sum(datum * np.log(x_predict) +
+                             (1. - datum) * np.log(1.0 - x_predict), axis=-1)
+            logpz = np.sum(np.log(p_vals), axis=-1)
+            logqz = np.sum(np.log(q_vals), axis=-1)
+            argsum = logp_xz + logpz - logqz
+            logpx = -np.log(num_samples) + logsumexp(argsum)
+            result.append(logpx)
+
+    return np.array(result)
